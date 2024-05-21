460a9c76e4d3607b8adcd0ca9656c70bab891ee0
Eleosse <jessy.allard84@gmail.com>
2024-05-21 21:57:14 +0200

    Parallax
    section 1 intro parallax mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,3 +6,14 @@ const menu = document.querySelector('.menu');
 menuBurger.addEventListener('click', function(){
     menu.classList.toggle('menu--open');
 })
+
+document.addEventListener('scroll', function() {
+    const layers = document.querySelectorAll('.parallax-layer');
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    layers.forEach((layer, index) => {
+        const depth = index * 0.24; // Change the depth factor as needed
+        const movement = -(scrollTop * depth);
+        layer.style.transform = `translateY(${movement}px)`;
+    });
+});
