4a5146efcb51aff3b9e3e507ac18679b01b41b0a
Eleosse <jessy.allard84@gmail.com>
2024-05-21 08:38:05 +0200

    Burger menu
    fonctionnel design à revoir

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,8 @@
 console.info('Hello world');
+
+const menuBurger = document.querySelector('.menu__burger');
+const menu = document.querySelector('.menu');
+
+menuBurger.addEventListener('click', function(){
+    menu.classList.toggle('menu--open');
+})
