fb90a3caa26b255daa02e2031cedc031fe867705
Eleosse <jessy.allard84@gmail.com>
2024-05-29 23:41:07 +0200

    section work + pop up
    pop up creer mais pas design + manque contenu dans pop up

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-console.info('Hello world');
+"use strict";
 
 const menuBurger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu');
@@ -7,13 +7,33 @@ menuBurger.addEventListener('click', function(){
     menu.classList.toggle('menu--open');
 })
 
-document.addEventListener('scroll', function() {
-    const layers = document.querySelectorAll('.parallax-layer');
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+const teamLinks = document.querySelectorAll(".project__link");
+teamLinks.forEach((el) => {
+    el.addEventListener("click", activeModal); 
+})
 
-    layers.forEach((layer, index) => {
-        const depth = index * 0.24; // Change the depth factor as needed
-        const movement = -(scrollTop * depth);
-        layer.style.transform = `translateY(${movement}px)`;
+function activeModal(event){
+    // empecher l'action du lien
+    event.preventDefault();
+    // var avec parent .team__el
+    let projectElement = this.closest(".project__el");
+    // ajoute la classe sur le parent team__el--active
+    projectElement.classList.add("project__el--active");
+    //var avec team__close qui se trouve DANS team__el--active
+    let projectClose = document.querySelector(".project__el--active .project__close");
+    //click sur team__close > remove team__el--active
+    projectClose.addEventListener("click", removeActive)
+    // var avec le team__overlay
+    let projectOverlay = document.querySelector(".project__el--active .project__overlay");
+    // click team__overlay > remove team__el--active
+    projectOverlay.addEventListener("click", removeActive)
+    // idem touche ESC
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            removeActive();
+        }
     });
-});
+    function removeActive(){
+        projectElement.classList.remove("project__el--active")
+    }
+}
