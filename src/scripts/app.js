ed3eebaebc02782809cf5553815ab99cb462b842
Eleosse <jessy.allard84@gmail.com>
2024-05-31 04:11:03 +0200

    work section contenu
    contenu rédiger manque image et vidéo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,21 +13,13 @@ teamLinks.forEach((el) => {
 })
 
 function activeModal(event){
-    // empecher l'action du lien
     event.preventDefault();
-    // var avec parent .team__el
     let projectElement = this.closest(".project__el");
-    // ajoute la classe sur le parent team__el--active
     projectElement.classList.add("project__el--active");
-    //var avec team__close qui se trouve DANS team__el--active
     let projectClose = document.querySelector(".project__el--active .project__close");
-    //click sur team__close > remove team__el--active
     projectClose.addEventListener("click", removeActive)
-    // var avec le team__overlay
     let projectOverlay = document.querySelector(".project__el--active .project__overlay");
-    // click team__overlay > remove team__el--active
     projectOverlay.addEventListener("click", removeActive)
-    // idem touche ESC
     document.addEventListener('keydown', function(event){
         if (event.key === 'Escape') {
             removeActive();
