3a3ec3bb29dfe86a91884a86132864e46477d29d
Eleosse <jessy.allard84@gmail.com>
2024-06-05 02:20:11 +0200

    save b4 storm
    save avant le responsive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -97,3 +97,24 @@ document.getElementById('checkbox2').addEventListener('change', function() {
     }});
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('.smooth-scroll');
+  
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+        const offsetTop = targetElement.offsetTop;
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
